6352ff1b3b3c0a281e541f5ba6ca829af02389d0
TygerImapct <tabanditkom@gmail.com>
2026-09-12 01:04:21 +0700

    Add Prodution

---
+++ Prodution/Interactive.js
@@ -0,0 +1,6 @@
+// ทำให้แถวบนเลื่อนไปทางหนึ่ง แถวล่างเลื่อนไปอีกทาง
+document.addEventListener("DOMContentLoaded", () => {
+  const clone = document.querySelector(".slide-track").cloneNode(true);
+  clone.style.animationDirection = "reverse";
+  document.querySelector(".slider").appendChild(clone);
+});
